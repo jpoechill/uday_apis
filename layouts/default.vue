e22cfced8e02ec21d10c16393645cebbf4d65f3e
Po Rith <poamrong@rith.com>
2020-09-08 14:07:11 -0700

    refine

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,24 @@
 <template>
   <div class="my-3">
-    <div class="container">
+    <!-- Header -->
+    <div class="fixed-top">
+      <div class="w-100 bg-dark text-white">
+        <div class="container bg-dark">
+          <div class="row py-2">
+            <div class="col-md-12 pt-2"> 
+              <span class="font-weight-bold" style="font-size: 24px;">
+                <nuxt-link to="/" class="text-white text-decoration-none">UD</nuxt-link>
+              </span>
+              <div class="float-right pt-1 dropdown d-inline">
+                jane.doe@udayhealth.org
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="container mb-4" style="margin-top: 80px;">
       <div class="row">
         <div class="col-md-12">
           Uday APIs: 
@@ -8,7 +26,8 @@
           <nuxt-link to="ha-features">HA Features</nuxt-link> | 
           <nuxt-link to="admin-features-01">Admin Features 1</nuxt-link> | 
           <nuxt-link to="admin-features-02">Admin Features 2</nuxt-link> | 
-          <nuxt-link to="admin-features-03">Admin Features 3</nuxt-link><br><br>
+          <nuxt-link to="admin-features-03">Admin Features 3</nuxt-link>
+          <hr>
         </div>
       </div>
     </div>
